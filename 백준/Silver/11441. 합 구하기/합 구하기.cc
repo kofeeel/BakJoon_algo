6439ead0e786ebe8fff3ca3b030a39edc54c9631
Hasimu <sinobu0211@naver.com>
2025-08-18 22:29:42 +0900

    [Silver III] Title: 합 구하기, Time: 36 ms, Memory: 2804 KB -BaekjoonHub

---
+++ 백준/Silver/11441. 합 구하기/합 구하기.cc
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+//ifdef Silver
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int N{}, M{};
+    cin >> N;
+
+    vector<int> numList(N);
+    vector<int> sumList(N + 1, 0);
+
+    for (int i = 0; i < N; i++)
+    {
+        cin >> numList[i]; 
+    }
+
+    for (int i = 0; i < N; i++)
+    {
+        sumList[i + 1] = sumList[i] + numList[i];
+    }
+
+    cin >> M;
+
+    while (M--)
+    {
+        int X, Y;
+        cin >> X >> Y;
+
+        cout << sumList[Y] - sumList[X - 1] << "\n";
+    }
+
+    return 0;
+}
+//endif
